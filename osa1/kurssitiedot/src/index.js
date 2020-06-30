256890d81e03114407859aabd95378681802d688
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-06-30 17:09:05 +0300

    1.4 Use objects from a table

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -10,60 +10,59 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
     </div>
   )
 }
 
-const Part = (props) => {
+const Part = (props) => { 
   return (
     <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
+      <p>{props.part.name} {props.part.exercises}</p>
     </div>
   )
 }
 
 const Total = (props) => {
+  let sum = 0
+  props.parts.forEach(p => {
+    sum += p.exercises
+  })
   return (
     <div>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {sum}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1.name}
-        exercises={part1.exercises}
-        part2={part2.name}
-        exercises2={part2.exercises}
-        part3={part3.name}
-        exercises3={part3.exercises}
+      <Content parts= {parts}
       />
-      <Total total = {part1.exercises + part2.exercises + part3.exercises} />
+      <Total parts = {parts} />
     </div>
   )
 }
