0f6d43b76e1b5527bd38a13a516b9e2ccb4bf295
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-07-01 19:56:06 +0300

    1.14 anekdootit step3

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -7,8 +7,24 @@ const Button = ({onClick, text}) => (
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  // https://stackoverflow.com/questions/20222501/how-to-create-a-zero-filled-javascript-array-of-arbitrary-length/22209781
   const [votes, setVotes] = 
   useState(Array.apply(null, new Array(10)).map(Number.prototype.valueOf,0))
+  const [topAnecdote, setTopAnecdote] = useState(0)
+
+  // muokattu lähteestä https://www.w3schools.com/js/js_array_sort.asp
+  const mostVotes = (arr) => {
+    let number = arr.length
+    let maxValue = -Infinity
+    let anecdote = 0
+    while (number--) {
+      if (arr[number] > maxValue) {
+        maxValue = arr[number]
+        anecdote = number
+      }
+    } 
+    setTopAnecdote(anecdote)   
+  }
 
   const handleSelectRandom = () => {
     let number
@@ -22,14 +38,19 @@ const App = (props) => {
     const copy = [...votes]
     copy[selected] += 1
     setVotes(copy)
+    mostVotes(copy)
   }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{props.anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <Button onClick={handleVote} text='vote'/>
       <Button onClick={handleSelectRandom} text='next anecdote'/>
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[topAnecdote]}</p>
+      <p>has {votes[topAnecdote]} votes</p>
     </div>
   )
 }
