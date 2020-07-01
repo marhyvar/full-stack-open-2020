cbf111f1ce956fa2d59831c75fc75b1a0675775a
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-07-01 10:20:28 +0300

    1.7 unicafe step2

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -6,6 +6,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (good - bad)/all
+  const positive = (good / all) * 100
 
   const handleGoodClick = () => setGood(good +1)
   const handleNeutralClick = () => setNeutral(neutral + 1)
@@ -17,10 +20,14 @@ const App = () => {
       <button onClick={handleGoodClick}>Good</button> 
       <button onClick={handleNeutralClick}>Neutral</button>
       <button onClick={handleBadClick}>Bad</button>
+      
       <h1>Statistics</h1>
       <p>Good {good}</p> 
       <p>Neutral {neutral}</p> 
       <p>Bad {bad}</p> 
+      <p>All {all}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive}%</p>
     </div>
   )
 }
