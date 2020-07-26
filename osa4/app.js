87d0ffe25f117a0aaeebab31994316c978c388a9
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-07-26 15:59:34 +0300

    4.2 blogilista step2, app omaan tiedostoon

---
+++ osa4/app.js
@@ -0,0 +1,23 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  // eslint-disable-next-line no-unused-vars
+  .then(result => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
