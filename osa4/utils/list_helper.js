d8388ef6924dc975f1b794791ed40b9ee72eee38
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-07-27 14:53:41 +0300

    4.5 apufunktioita ja yksikkötestejä step3

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -4,13 +4,30 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
+
   const reducer = (sum, blog) => {
     return sum + blog.likes
   }
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+
+  const reducer = (max, curr) => {
+    let bigger = max.likes > curr.likes ? max : curr
+    return {
+      title: bigger.title,
+      author: bigger.author,
+      likes: bigger.likes
+    }
+  }
+
+  return blogs.length === 0 ? 0
+    : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
