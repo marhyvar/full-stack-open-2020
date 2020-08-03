01ba871bc19538703dca40438be1b05c577247fa
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-08-03 13:50:34 +0300

    4.14 blogilistan laajennus step2

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -6,94 +6,122 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(helper.initialBlogs)
-})
+describe('when there are initial blogs saved', () => {
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
-
-test('the field identifying the blogs is called id', async () => {
-  const response = await api.get('/api/blogs')
-  response.body.map(r => {
-    expect(r.id).toBeDefined()
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
   })
-})
 
-test('blogs can be added', async () => {
-  const newBlog = {
-    title: 'React v16.8: The One With Hooks',
-    author: 'Dan Abramov',
-    url: 'https://reactjs.org/blog/2019/02/06/react-v16.8.0.html',
-    likes: 10,
-  }
+  describe('when retrieving blogs', () => {
+    test('blogs are returned as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    test('all blogs are returned', async () => {
+      const response = await api.get('/api/blogs')
+      expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
 
-  const blogsAfterAdding = await helper.blogsInDb()
-  expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length + 1)
-  const titles = blogsAfterAdding.map(b => b.title)
-  expect(titles).toContain(
-    'React v16.8: The One With Hooks'
-  )
-})
+    test('the field identifying the blogs is called id', async () => {
+      const response = await api.get('/api/blogs')
+      response.body.map(r => {
+        expect(r.id).toBeDefined()
+      })
+    })
+  })
+  describe('when adding a new blog', () => {
+    test('blogs can be added', async () => {
+      const newBlog = {
+        title: 'React v16.8: The One With Hooks',
+        author: 'Dan Abramov',
+        url: 'https://reactjs.org/blog/2019/02/06/react-v16.8.0.html',
+        likes: 10,
+      }
 
-test('if the likes of a blog has no value it is set to zero', async () => {
-  const newBlog = {
-    title: 'React v16.8: The One With Hooks',
-    author: 'Dan Abramov',
-    url: 'https://reactjs.org/blog/2019/02/06/react-v16.8.0.html'
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  const blogsAfterAdding = await helper.blogsInDb()
-  const likes = blogsAfterAdding[helper.initialBlogs.length].likes
-  expect(likes).toBe(0)
-})
+      const blogsAfterAdding = await helper.blogsInDb()
+      expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length + 1)
+      const titles = blogsAfterAdding.map(b => b.title)
+      expect(titles).toContain(
+        'React v16.8: The One With Hooks'
+      )
+    })
 
-test('blog without title and url cannot be added to list', async () => {
-  const newBlog = {
-    author: 'Dan Abramov'
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    test('if the likes of a blog has no value it is set to zero', async () => {
+      const newBlog = {
+        title: 'React v16.8: The One With Hooks',
+        author: 'Dan Abramov',
+        url: 'https://reactjs.org/blog/2019/02/06/react-v16.8.0.html'
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
 
-  const blogsAfterAdding = await helper.blogsInDb()
-  expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length)
-})
+      const blogsAfterAdding = await helper.blogsInDb()
+      const likes = blogsAfterAdding[helper.initialBlogs.length].likes
+      expect(likes).toBe(0)
+    })
 
-test('blog with valid id can be deleted', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToDelete = blogsAtStart[0]
-  console.log('id:', blogToDelete.id)
+    test('blog without title and url cannot be added to list', async () => {
+      const newBlog = {
+        author: 'Dan Abramov'
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
 
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
+      const blogsAfterAdding = await helper.blogsInDb()
+      expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length)
+    })
+  })
 
-  const blogsAfterDelete = await helper.blogsInDb()
-  expect(blogsAfterDelete.length).toBe(helper.initialBlogs.length -1)
+  describe('when deleting a blog', () => {
+    test('blog with valid id can be deleted', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+      console.log('id:', blogToDelete.id)
 
-  const titles = blogsAfterDelete.map(b => b.title)
-  expect(titles).not.toContain(blogToDelete.title)
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAfterDelete = await helper.blogsInDb()
+      expect(blogsAfterDelete.length).toBe(helper.initialBlogs.length -1)
+
+      const titles = blogsAfterDelete.map(b => b.title)
+      expect(titles).not.toContain(blogToDelete.title)
+    })
+  })
+
+  describe('when updating a blog', () => {
+    test('blog with valid id can be updated', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToUpdate = blogsAtStart[0]
+      const update = {
+        title: 'React v16.9.0 and the Roadmap Update',
+        author: 'Dan Abramov and Brian Vaughn',
+        url: 'https://reactjs.org/blog/2019/08/08/react-v16.9.0.html',
+        likes: 40
+      }
+      await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(update)
+
+      const blogsAfterUpdate = await helper.blogsInDb()
+      const likes = blogsAfterUpdate.map(b => b.likes)
+      expect(likes[0]).toBe(40)
+    })
+  })
 })
 
 afterAll(() => {
