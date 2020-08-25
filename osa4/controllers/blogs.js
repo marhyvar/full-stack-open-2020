53d0bd74f235cf4d8c0794fadd4f103175dba8b1
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-08-25 21:13:13 +0300

    4.17 blogilistan laajennus step6

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,13 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const user = await User.findById('5f339428613b323ca0e8c236')
 
   if (request.body.title === undefined || request.body.url === undefined) {
     return response.status(400).end()
@@ -17,9 +21,14 @@ blogsRouter.post('/', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes === undefined ? 0 : body.likes
+    likes: body.likes === undefined ? 0 : body.likes,
+    user: user._id
   })
+
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog.toJSON())
 })
 
--- osa4/models/user.js
+++ osa4/models/user.js
@@ -6,7 +6,13 @@ mongoose.set('useCreateIndex', true)
 const userSchema = mongoose.Schema({
   username: { type: String, minlength: 3, required: true, unique: true },
   name: String,
-  passwordHash: String
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ]
 })
 
 userSchema.plugin(uniqueValidator)
