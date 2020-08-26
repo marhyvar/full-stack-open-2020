b60a6b83da53cb7f0e5881c51e074f20c5d01c56
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-08-26 15:30:47 +0300

    4.21 blogilistan laajennus step10

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -37,8 +37,14 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const blog = await Blog.findById(request.params.id)
+  if ( blog.user.toString() === decodedToken.id.toString()) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } else {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
