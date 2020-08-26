5051a09a8def8cdd18e4ea4ea31776da80335821
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-08-26 14:21:37 +0300

    4.19 blogilistan laajennus step8

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -10,8 +19,12 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById('5f339428613b323ca0e8c236')
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (request.body.title === undefined || request.body.url === undefined) {
     return response.status(400).end()
