d30afffd0f26c1c2935ab1b606481e9110f6bf9c
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-09-24 16:44:50 +0300

    5.1 blogilistan frontend, step1

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      console.log(username, password)
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <LoginForm
+          username={ username }
+          setUsername={ setUsername }
+          password={ password }
+          setPassword={ setPassword }
+          handleLogin={ handleLogin }
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>{ user.name } logged in</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ osa5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const LoginForm = ({ username, setUsername, password, setPassword, handleLogin }) => {
+  return (
+      <div>
+        <form onSubmit={ handleLogin}>
+            <div>
+                username
+                  <input
+                  type='text'
+                  value={ username }
+                  name='Username'
+                  onChange={ ({ target }) => setUsername(target.value)}
+                  />
+            </div>
+            <div>
+                password
+                  <input
+                  type='password'
+                  value={ password }
+                  name='Password'
+                  onChange={ ({ target }) => setPassword(target.value)}
+                  />
+            </div>
+          <button type='submit'>login</button>
+        </form>
+      </div>
+  )
+}
+
+export default LoginForm
