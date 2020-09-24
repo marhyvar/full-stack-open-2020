4008ac07bb91176fcf26c24d9ed2ea1d8d7d7011
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-09-24 19:04:00 +0300

    5.2 blogilistan frontend, step2

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -16,6 +16,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInBlogUser')
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
@@ -23,12 +31,22 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedInBlogUser', JSON.stringify(user)
+      )
+
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log('wrong credentials')
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedInBlogUser')
+    setUser(null)
   }
 
   if (user === null) {
@@ -50,8 +68,9 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{ user.name } logged in</p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      <button onClick={ handleLogout }>logout</button>
+       {blogs.map(blog =>
+        <Blog key={ blog.id } blog={ blog } />
       )}
     </div>
   )
