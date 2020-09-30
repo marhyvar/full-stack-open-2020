9472ede73dc1f02d552c7ff022cff4bebbe973f8
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-09-30 18:55:07 +0300

    5.7 blogilistan frontend, step7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false)
+
+  const hideWhenVisible = { display: showAll ? 'none' : ''}
+  const showWhenVisible = { display: showAll ? '' : 'none'} 
+
+  const blogStyle = {
+    padding: 5,
+    border: 'solid',
+    marginBottom: 2
+  }
+
+  const blogStyle2 = {
+    padding: 10,
+    border: 'dotted',
+    marginBottom: 2,
+    background: 'lightyellow'
+  }
+
+  return (
+    <>
+      <div style={hideWhenVisible}>
+        <div style={blogStyle}>
+          {blog.title} {blog.author}
+          <button onClick={() => setShowAll(true)}>view</button>
+        </div>
+      </div>
+      <div style={showWhenVisible}>
+        <div style={blogStyle2}>
+          <p>{blog.title} {blog.author} <button onClick={() => setShowAll(false)}>hide</button></p>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes}<button>like</button></p>
+          { blog.user === undefined ? <p>anonymous</p> : <p>{ blog.user.username}</p>}
+        </div>
+      </div>
+    </>
+  )}
 
 export default Blog
