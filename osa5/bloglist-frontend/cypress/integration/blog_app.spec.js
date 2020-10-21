b219c3cd7d5e16a52ee27fd17a290ec96573bda1
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-10-21 14:02:31 +0300

    5.20 & 5.21 blogilistan end to end -testit, step4 & step5

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -6,7 +6,13 @@ describe('Blog app', function() {
       username: 'doggo',
       password: 'bones'
     }
+    const user2 = {
+      name: 'Mr. Cat',
+      username: 'kitty',
+      password: 'fish'
+    }
     cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.request('POST', 'http://localhost:3001/api/users/', user2)
     cy.visit('http://localhost:3000')
   })
 
@@ -51,6 +57,61 @@ describe('Blog app', function() {
       cy.get('button').contains('create').click()
 
       cy.get('.blog-list').contains('Introduction to Cypress')
+    })
+
+    it('A blog added by the user can be deleted by the same user', function() {
+      cy.createBlog({
+        title: 'The Go blog',
+        author: 'Gophers',
+        url: 'https://blog.golang.org/'
+      })
+      cy.createBlog({
+        title: 'Go 1.15 is released',
+        author: 'Gophers',
+        url: 'https://blog.golang.org/go1.15/'
+      })
+      cy.contains('The Go blog Gophers').contains('view').click()
+      cy.get('.moreInfo').contains('The Go blog Gophers').parent().contains('remove').click()
+      cy.contains('blog The Go blog by Gophers was deleted')
+      cy.get('.lessInfo').should('have.length', 1)
+    })
+
+    it('A blog added by the user can not be deleted by another user', function() {
+      cy.createBlog({
+        title: 'The Go blog',
+        author: 'Gophers',
+        url: 'https://blog.golang.org/'
+      })
+      cy.createBlog({
+        title: 'Go 1.15 is released',
+        author: 'Gophers',
+        url: 'https://blog.golang.org/go1.15/'
+      })
+
+      cy.contains('logout').click()
+      cy.login({ username: 'kitty', password: 'fish' })
+      cy.contains('The Go blog Gophers').contains('view').click()
+      cy.get('.moreInfo').contains('The Go blog Gophers').parent()
+        .contains('remove').should('not.exist')
+    })
+
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'The Go blog',
+          author: 'Gophers',
+          url: 'https://blog.golang.org/'
+        })
+      })
+
+      it('it can be liked', function () {
+        cy.contains('The Go blog Gophers').contains('view').click()
+
+        cy.contains('like')
+          .click()
+
+        cy.get('.moreInfo').contains('The Go blog Gophers').parent().contains('likes 1')
+      })
     })
   })
 })
